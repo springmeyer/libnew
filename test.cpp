698423e2b9e6e0cbebccc630ab8b68f17072c128
Dane Springmeyer <dane@mapbox.com>
2017-01-24 12:58:29 -0800

    test calloc

--- test.cpp
+++ test.cpp
@@ -3,14 +3,14 @@
 #include <dlfcn.h>
 #include <cstdlib>
 
-int new_count(void * handle) {
+int _new_count(void * handle) {
     using callable_type = int (*) ();
     callable_type libnew_get_new_count = reinterpret_cast<callable_type>(dlsym(handle, "get_new_count"));
     if (!libnew_get_new_count) throw std::runtime_error("get_new_count not found");
     return libnew_get_new_count();
 }
 
-int malloc_count(void * handle) {
+int _malloc_count(void * handle) {
     using callable_type = int (*) ();
     callable_type libnew_get_malloc_count = reinterpret_cast<callable_type>(dlsym(handle, "get_malloc_count"));
     if (!libnew_get_malloc_count) throw std::runtime_error("get_malloc_count not found");
@@ -36,30 +36,38 @@ int main() {
             throw std::runtime_error("Must provide LD_PRELOAD on linux and DYLD_INSERT_LIBRARIES on OS X");
         }
 
-        std::clog << "nc1: " << new_count(handle) << "\n";
-        std::clog << "mc1: " << malloc_count(handle) << "\n";
+        std::clog << "nc1: " << _new_count(handle) << "\n";
+        std::clog << "mc1: " << _malloc_count(handle) << "\n";
         {
             std::clog << "long std::string\n";
             std::string s("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
         }
 
-        std::clog << "nc2: " << new_count(handle) << "\n";
-        std::clog << "mc2: " << malloc_count(handle) << "\n";
+        std::clog << "nc2: " << _new_count(handle) << "\n";
+        std::clog << "mc2: " << _malloc_count(handle) << "\n";
         {
             std::clog << "20 item unsigned int array\n";
             std::unique_ptr<unsigned int[]> out_row(new unsigned int[20]);
         }
 
-        std::clog << "nc3: " << new_count(handle) << "\n";
-        std::clog << "mc3: " << malloc_count(handle) << "\n";
+        std::clog << "nc3: " << _new_count(handle) << "\n";
+        std::clog << "mc3: " << _malloc_count(handle) << "\n";
         {
             std::clog << "direct malloc of unsigned int pointer\n";
             unsigned int *ptr_one = (unsigned int *)malloc(sizeof(unsigned int));
             free(ptr_one);
         }
 
-        std::clog << "nc4: " << new_count(handle) << "\n";
-        std::clog << "mc4: " << malloc_count(handle) << "\n";
+        std::clog << "nc4: " << _new_count(handle) << "\n";
+        std::clog << "mc4: " << _malloc_count(handle) << "\n";
+
+        {
+           int* p1 = (int*)std::calloc(4, sizeof(int));
+           free(p1);
+        }
+
+        std::clog << "nc5: " << _new_count(handle) << "\n";
+        std::clog << "mc5: " << _malloc_count(handle) << "\n";
     } catch (std::exception const& ex) {
         std::clog << "Error: " << ex.what() << "\n";
         return -1;
