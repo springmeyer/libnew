0264c9bcdb2da43d11631fd7290f837839a51bd1
Dane Springmeyer <dane@mapbox.com>
2017-01-24 12:58:16 -0800

    optimize by caching result of dlsym

--- libnew.cpp
+++ libnew.cpp
@@ -9,53 +9,83 @@
 #include <stdio.h>
 
 static std::size_t new_max = 0;
-static std::size_t malloc_max = 0;
-static std::size_t calloc_max = 0;
+static std::size_t new_total = 0;
 static std::size_t new_count = 0;
 static std::size_t delete_count = 0;
+const int max_size_new_debug = std::stoi(std::getenv("NEW_DEBUG_SIZE") ? std::getenv("NEW_DEBUG_SIZE") : "0" );
+const int max_size_new_abort = std::stoi(std::getenv("NEW_ABORT_SIZE") ? std::getenv("NEW_ABORT_SIZE") : "0" );
+
+static std::size_t malloc_max = 0;
+static std::size_t malloc_total = 0;
 static std::size_t malloc_count = 0;
 static std::size_t free_count = 0;
+const int max_size_malloc_debug = std::stoi(std::getenv("MALLOC_DEBUG_SIZE") ? std::getenv("MALLOC_DEBUG_SIZE") : "0" );
+const int max_size_malloc_abort = std::stoi(std::getenv("MALLOC_ABORT_SIZE") ? std::getenv("MALLOC_ABORT_SIZE") : "0" );
+
 static std::size_t calloc_count = 0;
+static std::size_t calloc_total = 0;
+static std::size_t calloc_max = 0;
 
 void atexit_handler() 
 {
-    std::clog << "new:              " << new_count << "\n";
-    std::clog << " new max size:    " << new_max << "\n";
-    std::clog << " delete:          " << delete_count << "\n";
-    std::clog << "malloc:           " << malloc_count << "\n";
-    std::clog << " malloc max size: " << malloc_max << "\n";
-    std::clog << " free:            " << free_count << "\n";
-    std::clog << "calloc:           " << calloc_count << "\n";
-    std::clog << " calloc max size: " << calloc_max << "\n";
+    std::clog << "new:\n";
+    std::clog << "  count:    " << new_count << "\n";
+    std::clog << "  total:    " << new_total << "\n";
+    std::clog << "  max:      " << new_max << "\n";
+    std::clog << "  deletes:  " << delete_count << "\n";
+    std::clog << "\n";
+    std::clog << "malloc:\n";
+    std::clog << "  count:    " << malloc_count << "\n";
+    std::clog << "  total:    " << malloc_total << "\n";
+    std::clog << "  max:      " << malloc_max << "\n";
+    std::clog << "  free:     " << free_count << "\n";
+    std::clog << "\n";
+    std::clog << "calloc:\n";
+    std::clog << "  count:    " << calloc_count << "\n";
+    std::clog << "  total:    " << calloc_total << "\n";
+    std::clog << "  max:      " << calloc_max << "\n";
 }
  
 const int result_1 = std::atexit(atexit_handler);
-
-const int max_size_new_debug = std::stoi(std::getenv("NEW_DEBUG_SIZE") ? std::getenv("NEW_DEBUG_SIZE") : "0" );
-const int max_size_malloc_debug = std::stoi(std::getenv("MALLOC_DEBUG_SIZE") ? std::getenv("MALLOC_DEBUG_SIZE") : "0" );
-const int max_size_new_abort = std::stoi(std::getenv("NEW_ABORT_SIZE") ? std::getenv("NEW_ABORT_SIZE") : "0" );
-const int max_size_malloc_abort = std::stoi(std::getenv("MALLOC_ABORT_SIZE") ? std::getenv("MALLOC_ABORT_SIZE") : "0" );
 
 extern "C" {
 
 int get_new_count() { return new_count; }
 int get_malloc_count() { return malloc_count; }
 
+using calloc_type = void * (*) (size_t, size_t);
+static void* (*real_calloc)(size_t nmemb, size_t size)=nullptr;
+
 void* calloc(size_t num, size_t size)
 {
+    if(real_calloc == nullptr) {
+        real_calloc = reinterpret_cast<calloc_type>(dlsym(RTLD_NEXT, "calloc"));
+        if (real_calloc == nullptr) {
+          abort();
+        }
+    }
+
     ++calloc_count;
+    calloc_total += size;
     if (size > calloc_max) calloc_max = size;
-    using callable_type = void * (*) (size_t, size_t);
-    callable_type libc_calloc = reinterpret_cast<callable_type>(dlsym(RTLD_NEXT, "calloc"));
-    return libc_calloc(num,size);
+    return real_calloc(num,size);
 }
+
+using malloc_type = void * (*) (size_t);
+static void* (*real_malloc)(size_t size)=nullptr;
 
 void* malloc(size_t size)
 {
+    if(real_malloc == nullptr) {
+        real_malloc = reinterpret_cast<malloc_type>(dlsym(RTLD_NEXT, "malloc"));
+        if (real_malloc == nullptr) {
+          abort();
+        }
+    }
+
     ++malloc_count;
+    malloc_total += size;
     if (size > malloc_max) malloc_max = size;
-    using callable_type = void * (*) (size_t);
-    callable_type libc_malloc = reinterpret_cast<callable_type>(dlsym(RTLD_NEXT, "malloc"));
     if (max_size_malloc_debug > 0  && size >= max_size_malloc_debug) {
         std::fprintf(stderr, "malloc %lu %s %s:%d\n", size,__FUNCTION__, __FILE__, __LINE__);
         void* callstack[128];
@@ -65,21 +95,31 @@ void* malloc(size_t size)
     if (max_size_malloc_abort > 0 && size >= max_size_malloc_abort) {
         abort();
     }
-    return libc_malloc(size);
+    return real_malloc(size);
 }
 
-void free(void *p)
+
+using free_type = void (*) (void *);
+static void (*real_free)(void * p)=nullptr;
+
+void free(void * p)
 {
+    if(real_free == nullptr) {
+        real_free = reinterpret_cast<free_type>(dlsym(RTLD_NEXT, "free"));
+        if (real_free == nullptr) {
+          abort();
+        }
+    }
+
     ++free_count;
-    using callable_type = void (*) (void *);
-    callable_type libc_free = reinterpret_cast<callable_type>(dlsym(RTLD_NEXT, "free"));
-    libc_free(p);
+    return real_free(p);
 }
 
 }
 
 void* operator new(std::size_t size) {
     ++new_count;
+    new_total += size;
     if (size > new_max) new_max = size;
     void *p = std::malloc(size);
     if (p == 0) { // did malloc succeed?
